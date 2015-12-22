21e33b1e4333810bb5c01c253448cbdaa19795c9
chenzhipei <chenzhipei@hotoa.com>
2015-12-22 11:59:15 +0800

    start_server.bat的修改

--- start_server.bat
+++ start_server.bat
@@ -9,12 +9,6 @@ set PHP_FCGI_MAX_REQUESTS=1000
 :: Start PHP-fastcgi on port 9000
 RunHiddenConsole php\php-cgi.exe -b 127.0.0.1:9000 -c php\php.ini
 
-:: Start MySQL using the mysql\my.ini config file
-start mysql\bin\mysqld
-
-:: Start Memcached (m = memory to use in MB, c = max connections allowed)
-RunHiddenConsole memcached\memcached.exe -m 10 -c 1024
-
 :: Start the nginx server
 cd nginx
 start nginx
